00041104b2b51f0ce13afe75ffde7662d2805eff
DarZz1 <104559023+DarZz1@users.noreply.github.com>
2026-04-02 19:58:36 +0300

    Swiper

---
+++ JS/carousel.js
@@ -0,0 +1,17 @@
+const swiper = new Swiper('.mySwiper', {
+    direction: 'horizontal',
+    loop: true,
+
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+
+    slidesPerView: 2, 
+    spaceBetween: 10,
+  });
