3f9080933634d22d1dc0fd5fc52b844242d3dd19
DarZz1 <104559023+DarZz1@users.noreply.github.com>
2026-04-02 21:34:12 +0300

    swiper

--- JS/carousel.js
+++ JS/carousel.js
@@ -1,17 +1,40 @@
+
 const swiper = new Swiper('.gallerySwiper', {
     direction: 'horizontal',
     loop: true,
+    slidesPerView: 1,
+    spaceBetween: 10,
 
     navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
     },
 
     pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
+        el: '.swiper-pagination',
+        clickable: true,
     },
 
-    slidesPerView: 5,
-    spaceBetween: 20,
-  });
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+            spaceBetween: 10,
+        },
+        577: {
+            slidesPerView: 2,
+            spaceBetween: 15,
+        },
+        768: {
+            slidesPerView: 3,
+            spaceBetween: 20,
+        },
+        992: {
+            slidesPerView: 4,
+            spaceBetween: 20,
+        },
+        1200: {
+            slidesPerView: 5,
+            spaceBetween: 20,
+        }
+    },
+});
