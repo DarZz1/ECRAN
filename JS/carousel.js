982f310b99cfeb45f6b929e599c9008802f3d2a9
DarZz1 <104559023+DarZz1@users.noreply.github.com>
2026-04-03 14:51:54 +0300

    swiper partners + news

--- JS/carousel.js
+++ JS/carousel.js
@@ -1,4 +1,4 @@
-const swiper = new Swiper('.gallerySwiper', {
+const gallerySwiper = new Swiper('.gallerySwiper', {
     direction: 'horizontal',
     loop: true,
     slidesPerView: 1,
@@ -10,7 +10,7 @@ const swiper = new Swiper('.gallerySwiper', {
     },
 
     pagination: {
-        el: '.swiper-pagination', // Должно совпадать с классом в HTML
+        el: '.swiper-pagination',
         clickable: true,
     },
 
@@ -29,6 +29,76 @@ const swiper = new Swiper('.gallerySwiper', {
         },
         1200: {
             slidesPerView: 5,
+        }
+    },
+});
+
+const exampleSwiper = new Swiper('.exampleSwiper', {
+    direction: 'horizontal',
+    loop: true,
+    slidesPerView: 1,
+    spaceBetween: 24,
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+
+    breakpoints: {
+        577: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        992: {
+            slidesPerView: 3,
+        }
+    },
+});
+
+const partnersSwiper = new Swiper ('.partnersSwiper', {
+    direction: 'horizontal',
+    loop: true,
+    slidesPerView: 1,
+    spaceBetween: 20,
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+
+    breakpoints: {
+        0: {
+            slidesPerView: 2,
+        },
+        577: {
+            slidesPerView: 3,
+            spaceBetween: 12,
+        },
+        768: {
+            slidesPerView: 4,
+            spaceBetween: 20,
+        },
+        992: {
+            slidesPerView: 5,
+            spaceBetween: 32,
+        },
+        1200: {
+            slidesPerView: 6,
+        },
+        1400: {
+            slidesPerView: 7,
         }
     },
 });
